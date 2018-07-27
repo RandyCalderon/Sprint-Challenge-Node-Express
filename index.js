e6739b22c5b98e12e5b77a9cc09320328c202dca
Randy Calderon <randycweb@gmail.com>
2018-07-27 09:45:13 -0700

    Added put for actions

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const server = express()
-server.use(express.json())
 
 const actionModel = require('./data/helpers/actionModel')
 const projectModel = require('./data/helpers/projectModel')
 const mappers = require('./data/helpers/mappers')
 
+server.use(express.json())
 
 // projectModel
 server.get('/api/projects', async (req, res) => {
@@ -33,8 +33,7 @@ server.get('/api/projects/:id', async (req, res) => {
 
 server.post('api/projects', async (req, res) => {
     try {
-        const { name } = req.body
-        const projectInsert = await projectModel.insert({name})
+        const projectInsert = await projectModel.insert(req.body)
         res.status(200).json(projectInsert)
     } catch(err) {
         
@@ -60,6 +59,29 @@ server.get('/api/actions/:id', async (req, res) => {
         res.status(200).json(actionGetId)
     } catch(err) {
         res.status(500).json({error: "Unable to retrieve action data"})
+    }
+})
+
+server.post('/api/actions', async (req, res) => {
+    try {
+        const insertAction = await actionModel.insert(req.body)
+        res.status(200).json(insertAction)
+    } catch(err) {
+
+    }
+})
+
+server.put('api/actions/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const {project_id, description, notes} = req.body
+        if (project_id === undefined || description === undefined) {
+            return res.status(400).json({error: "Please provide Id/Description or both if missing"})
+        }
+        const updateAction = await actionModel.update(id, {project_id, description, notes })
+        res.status(200).json(updateAction)
+    } catch(err) {
+
     }
 })
 server.listen(8000)
